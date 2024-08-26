35e80b3d90a247a517af331686c6c3acba7e33a0
MurphyHou <houyafeng.vip@foxmail.com>
2024-08-26 22:08:52 +0800

    feat: useEffect概念

---
+++ src/entry_level02/App03.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const URL = 'http://geek.itheima.net/v1_0/channels'
+
+function App() {
+  const [list, setList] = useState([])
+  useEffect(() => {
+    async function getList() {
+      const res = await fetch(URL)
+      const jsonRes = await res.json()
+      setList(jsonRes.data.channels)
+    }
+    getList()
+  }, [])
+  return (
+    <div>
+      App
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
